957554eeb8e4cd7c49f3dc508b3a9d578d1f605c
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-06 20:01:35 -0500

    Added an Embedable Address class for shipping/billing address; updated table schema; updated equals and hashcode

--- src/main/java/com/blurdel/sdjpa/orderservice/domain/OrderHeader.java
+++ src/main/java/com/blurdel/sdjpa/orderservice/domain/OrderHeader.java
@@ -3,6 +3,9 @@ package com.blurdel.sdjpa.orderservice.domain;
 import java.util.Objects;
 
 import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,9 +13,46 @@ import jakarta.persistence.Id;
 
 
 @Entity
+@AttributeOverrides({
+		@AttributeOverride(
+				name = "shippingAddress.address",
+				column = @Column(name = "shipping_address")
+		),
+		@AttributeOverride(
+				name = "shippingAddress.city",
+				column = @Column(name = "shipping_city")
+		),
+		@AttributeOverride(
+				name = "shippingAddress.state",
+				column = @Column(name = "shipping_state")
+		),
+		@AttributeOverride(
+				name = "shippingAddress.zipCode",
+				column = @Column(name = "shipping_zip_code")
+		),
+		@AttributeOverride(
+				name = "billToAddress.address",
+				column = @Column(name = "bill_to_address")
+		),
+		@AttributeOverride(
+				name = "billToAddress.city",
+				column = @Column(name = "bill_to_city")
+		),
+		@AttributeOverride(
+				name = "billToAddress.state",
+				column = @Column(name = "bill_to_state")
+		),
+		@AttributeOverride(
+				name = "billToAddress.zipCode",
+				column = @Column(name = "bill_to_zip_code")
+		)
+})
 public class OrderHeader extends BaseEntity {
 	
 	private String customer;
+	private Address shippingAddress;
+	private Address billToAddress;
+
 
 	public String getCustomer() {
 		return customer;
@@ -22,18 +62,34 @@ public class OrderHeader extends BaseEntity {
 		this.customer = customer;
 	}
 
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+	public Address getBillToAddress() {
+		return billToAddress;
+	}
+
+	public void setBillToAddress(Address billToAddress) {
+		this.billToAddress = billToAddress;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		if (!super.equals(o)) return false;
 		OrderHeader that = (OrderHeader) o;
-		return Objects.equals(customer, that.customer);
+		return Objects.equals(customer, that.customer) && Objects.equals(shippingAddress, that.shippingAddress) && Objects.equals(billToAddress, that.billToAddress);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(super.hashCode(), customer);
+		return Objects.hash(super.hashCode(), customer, shippingAddress, billToAddress);
 	}
 
 }
--- src/main/resources/my-schema.sql
+++ src/main/resources/my-schema.sql
@@ -1,6 +1,14 @@
 create table order_header
 (
     id        bigint not null auto_increment primary key,
-    customer      varchar(255)
+    customer      varchar(255),
+    shipping_address varchar(30),
+    shipping_city varchar(30),
+    shipping_state varchar(30),
+    shipping_zip_code varchar(30),
+    bill_to_address varchar(30),
+    bill_to_city varchar(30),
+    bill_to_state varchar(30),
+    bill_to_zip_code varchar(30)
 ) engine = InnoDB;
 
