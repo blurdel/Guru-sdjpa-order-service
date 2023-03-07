a6a642aaf1f070e0c666875028a167789158dd09
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-06 19:43:25 -0500

    Added a BaseEntity class as a MappedSuperclass

--- src/main/java/com/blurdel/sdjpa/orderservice/domain/OrderHeader.java
+++ src/main/java/com/blurdel/sdjpa/orderservice/domain/OrderHeader.java
@@ -2,6 +2,7 @@ package com.blurdel.sdjpa.orderservice.domain;
 
 import java.util.Objects;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,21 +10,9 @@ import jakarta.persistence.Id;
 
 
 @Entity
-public class OrderHeader {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class OrderHeader extends BaseEntity {
 	
 	private String customer;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getCustomer() {
 		return customer;
@@ -34,25 +23,17 @@ public class OrderHeader {
 	}
 
 	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
+		OrderHeader that = (OrderHeader) o;
+		return Objects.equals(customer, that.customer);
+	}
+
+	@Override
 	public int hashCode() {
-		return Objects.hash(customer, id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		OrderHeader other = (OrderHeader) obj;
-		return Objects.equals(customer, other.customer) && Objects.equals(id, other.id);
-	}
-
-	@Override
-	public String toString() {
-		return "OrderHeader [id=" + id + ", customer=" + customer + "]";
+		return Objects.hash(super.hashCode(), customer);
 	}
 
 }
