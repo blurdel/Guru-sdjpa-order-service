9d5cac8df93f20e60157edeef3f8e956df07676b
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-16 19:00:53 -0400

    Added orphanRemaoval on OrderHeader.orderApproval to remove the approval on delete, but theres a better way ...

--- src/test/java/com/blurdel/sdjpa/repositories/OrderHeaderRepositoryTest.java
+++ src/test/java/com/blurdel/sdjpa/repositories/OrderHeaderRepositoryTest.java
@@ -130,6 +130,10 @@ public class OrderHeaderRepositoryTest {
         orderLine.setQuantityOrdered(3);
         orderLine.setProduct(product);
 
+        OrderApproval orderApproval = new OrderApproval();
+        orderApproval.setApprovedBy("me");
+        orderHeader.setOrderApproval(orderApproval);
+
         orderHeader.addOrderLine(orderLine);
         OrderHeader savedOrder = orderHeaderRepo.saveAndFlush(orderHeader);
 
